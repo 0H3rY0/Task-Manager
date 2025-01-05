f6c353c44f6b828c6196007c56830936eebf6f2e
Szymon <szymers12345@gmail.com>
2025-01-05 22:28:58 +0100

    add tasks list and working delete button

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,9 +1,27 @@
 import { CgGoogleTasks } from "react-icons/cg";
 import { IoMdAdd } from "react-icons/io";
-import { useRef, useState } from "react";
+import { useState } from "react";
+import { TiDelete } from "react-icons/ti";
+import { RxUpdate } from "react-icons/rx";
 
 const Tasks = () => {
-  const inputRef = useRef(null);
+  const [tasks, setTasks] = useState([]);
+  const [inputText, setInputText] = useState("");
+
+  const addTask = () => {
+    setTasks([...tasks, inputText]);
+    setInputText("");
+  };
+
+  const removeTask = (index) => {
+    setTasks(tasks.filter((item, i) => i !== index));
+  };
+
+  const handleEnterPress = (event) => {
+    if (event.key === "Enter") {
+      addTask(inputText);
+    }
+  };
 
   return (
     <div className="w-4/5 flex justify-center py-16  flex-col gap-6">
@@ -11,13 +29,42 @@ const Tasks = () => {
         Tasks <CgGoogleTasks className="text-blue-500" size={32} />
       </h2>
       <div className="flex gap-3">
-        <IoMdAdd size={32} className="text-red-500" />
+        <IoMdAdd size={32} className="text-red-500" onClick={addTask} />
         <input
-          ref={inputRef}
+          onChange={(e) => setInputText(e.target.value)}
+          value={inputText}
+          onKeyDown={handleEnterPress}
           type="text"
           placeholder="Add task"
           className="w-full px-3 rounded-full focus:border-white focus:ring-2 focus:ring-red-300 focus:outline-none transition"
         />
+      </div>
+      <div>
+        <ul className="flex flex-col">
+          {tasks.map((item, index) => {
+            return (
+              <>
+                <div className="flex items-center border-2 border-slate-200 mt-5 rounded-md">
+                  <li
+                    className="py-4 px-2 
+                text-lg font-semibold text-slate-700 w-4/5"
+                    key={index}
+                  >
+                    {item}
+                  </li>
+                  <div className=" w-1/5 py-4 px-2 flex items-center gap-5 justify-end mr-5">
+                    <TiDelete
+                      className="text-red-500"
+                      size={40}
+                      onClick={() => removeTask(index)}
+                    />
+                    <RxUpdate className="text-purple-500" size={32} />
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
