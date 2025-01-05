9566425def2b28f8132b9ed4abcdf484e2dce521
Szymon <szymers12345@gmail.com>
2025-01-05 18:50:54 +0100

    navbar base component structure with working animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import { useState } from "react";
 
 function App() {
-  return <Header />;
+  const [navbarActive, setNavbarActive] = useState(false);
+
+  return (
+    <>
+      <Header setNavbarActive={setNavbarActive} navbarActive={navbarActive} />
+      {navbarActive && <Navbar navbarActive={navbarActive} />}
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = ({ navbarActive }) => {
+  return (
+    <div
+      className={`w-[300px] absolute top-0 left-0 h-full
+      shadow-xl slide-right`}
+    >
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
