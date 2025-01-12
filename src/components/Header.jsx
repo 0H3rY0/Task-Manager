9a03d8a7d8e6887cc28bdd5387ec899eaed1c9f8
Szymon <szymers12345@gmail.com>
2025-01-12 19:23:48 +0100

    navbar and header are fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ function Header({ setNavbarActive, navbarActive }) {
     <header
       className={
         `py-2 sm:py-4  px-3 sm:px-6 bg-red-500 
-    flex justify-between ` +
+    flex justify-between sticky top-0 ` +
         `${
           navbarActive
             ? "md:ml-[300px] headerAnimation"
