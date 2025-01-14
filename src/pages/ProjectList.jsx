b50f86f207d50b095beac3c220b45192d8f387de
Szymon <szymers12345@gmail.com>
2025-01-15 00:50:23 +0100

    add logic of prevent from loading errors and add spiner to ProjectList

--- src/pages/ProjectList.jsx
+++ src/pages/ProjectList.jsx
@@ -4,14 +4,22 @@ import { NavLink } from "react-router";
 import { IoMdAdd } from "react-icons/io";
 import { useEffect, useState } from "react";
 import ProjectService from "../service/api/projects";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const ProjectList = () => {
   const [projectsList, setProjectsList] = useState([]);
+  const [spinnserLoader, setSpinnerLoader] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const getAllProjects = async () => {
-      const data = await ProjectService.getAll();
-      setProjectsList(data);
+      try {
+        const data = await ProjectService.getAll();
+        data.length > 0 ? setProjectsList(data) : setSpinnerLoader(false);
+      } catch (error) {
+        console.log("project load failed: " + error);
+        setError(true);
+      }
     };
 
     getAllProjects();
@@ -31,52 +39,72 @@ const ProjectList = () => {
       </div>
 
       <div>
-        <ul className="flex flex-col gap-6">
-          {projectsList.map((item, index) => (
-            <li
-              key={index}
-              className=" py-4 px-4 rounded-md flex items-center min-h-30 shadow-md"
-            >
-              <div className="flex justify-center items-center w-1/5">
-                {item.ImageUrl !== "" ? (
-                  <img
-                    className="w-[100px] h-[100px]"
-                    src={item.ImageUrl}
-                  ></img>
-                ) : (
-                  <SiTask size={70} className="text-orange-500" />
-                )}
-              </div>
-              <div className="w-3/5 flex flex-col gap-3">
-                <div>
-                  <h3 className="font-bold text-2xl text-slate-700 ">
-                    {item.Title}
-                  </h3>
-                  <p className="font-semibold text-lg text-slate-500 ">
-                    {item.Description}
+        {projectsList.length > 0 ? (
+          <ul className="flex flex-col gap-6">
+            {projectsList.map((item, index) => (
+              <li
+                key={index}
+                className=" py-4 px-4 rounded-md flex items-center min-h-30 shadow-md"
+              >
+                <div className="flex justify-center items-center w-1/5">
+                  {item.ImageUrl !== "" ? (
+                    <img
+                      className="w-[100px] h-[100px]"
+                      src={item.ImageUrl}
+                    ></img>
+                  ) : (
+                    <SiTask size={70} className="text-orange-500" />
+                  )}
+                </div>
+                <div className="w-3/5 flex flex-col gap-3">
+                  <div>
+                    <h3 className="font-bold text-2xl text-slate-700 ">
+                      {item.Title}
+                    </h3>
+                    <p className="font-semibold text-lg text-slate-500 ">
+                      {item.Description}
+                    </p>
+                  </div>
+                  <div>
+                    <NavLink to={`/project/${item.id}`}>
+                      <button className="btn-gray px-24">Watch more</button>
+                    </NavLink>
+                  </div>
+                </div>
+                <div className="flex flex-col justify-end items-end w-1/5 gap-14">
+                  <p className="flex gap-1 font-semibold text-slate-700">
+                    Importance:{" "}
+                    <span className="text-orange-500">{item.Importance}</span>
+                  </p>
+                  <p className="flex gap-1 font-semibold text-slate-700">
+                    DeadLine:{" "}
+                    <span className="text-orange-500 whitespace-nowrap">
+                      {item.Deadline}
+                    </span>
                   </p>
                 </div>
-                <div>
-                  <NavLink to={`/project/${item.id}`}>
-                    <button className="btn-gray px-24">Watch more</button>
-                  </NavLink>
-                </div>
-              </div>
-              <div className="flex flex-col justify-end items-end w-1/5 gap-14">
-                <p className="flex gap-1 font-semibold text-slate-700">
-                  Importance:{" "}
-                  <span className="text-orange-500">{item.Importance}</span>
-                </p>
-                <p className="flex gap-1 font-semibold text-slate-700">
-                  DeadLine:{" "}
-                  <span className="text-orange-500 whitespace-nowrap">
-                    {item.Deadline}
-                  </span>
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
+            {error ? (
+              // <ClipLoader className="text-center" color="#ff6300" size={150} />
+              <p className="font-bold text-2xl text-slate-500 text-center">
+                Project Failed to Load{" "}
+              </p>
+            ) : spinnserLoader ? (
+              <ClipLoader className="text-center" color="#ff6300" size={150} />
+            ) : (
+              // <p className="font-bold text-2xl text-slate-500 text-center">
+              //   Project Failed to Load{" "}
+              // </p>
+              <p className="font-bold text-2xl text-slate-500 text-center">
+                No tasks Added yet
+              </p>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
