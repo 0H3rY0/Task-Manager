c1fde69a2e97cf6d4552a93aeca43d91a2b47028
Szymon <szymers12345@gmail.com>
2025-01-18 23:02:22 +0100

    change projectSchema validation for max length description

--- src/utils/projectSchema.js
+++ src/utils/projectSchema.js
@@ -2,7 +2,9 @@ import * as Yup from "yup";
 
 export let projectSchema = Yup.object({
   Title: Yup.string().required("Title is required"),
-  Description: Yup.string().required("Description is required"),
+  Description: Yup.string()
+    .required("Description is required")
+    .max(100, "Description cannot exceed 100 characters"),
   Deadline: Yup.date()
     .nullable()
     .transform((value, originalValue) => (originalValue === "" ? null : value))
