c8c59d0776974bebde40228e840005451a8338ea
Szymon <szymers12345@gmail.com>
2025-02-02 23:05:36 +0100

    completed switch buttons works correctly

--- src/components/ui/TasksList.jsx
+++ src/components/ui/TasksList.jsx
@@ -52,11 +52,7 @@ const TasksList = ({
       },
     };
     setTaskStatus(e.target.checked);
-    ProjectService.updateProjectTask(
-      newTask.id,
-      "871387ba-f178-490f-8c00-2b71aada6d59",
-      newTask
-    );
+    modifyTask(taskId, newTask);
   };
 
   console.log(tasks);
@@ -156,7 +152,6 @@ const TasksList = ({
                         type="checkbox"
                         className="sr-only peer"
                         defaultChecked={item.status.done}
-                        // checked={item.status.done}
                         onChange={(e) => updateTasksStatus(e, item.id, item)}
                       />
                       <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-500 dark:peer-checked:bg-green-500"></div>
