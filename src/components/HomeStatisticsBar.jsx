044c042d5d3053cc8bd92a912f62b8fdeea3656f
Szymon <szymers12345@gmail.com>
2025-02-02 20:37:54 +0100

    HomeStatisticsBar current date display support

--- src/components/HomeStatisticsBar.jsx
+++ src/components/HomeStatisticsBar.jsx
@@ -1,11 +1,18 @@
 import { IoCheckmarkSharp } from "react-icons/io5";
 import { MdOutlinePending } from "react-icons/md";
 import OwnSelectDayList from "./ui/OwnSelectDayList";
+import { days } from "../service/data/days";
+import { months } from "../service/data/months";
 
 const HomeStatisticsBar = () => {
+  const todayDate = new Date();
+  const day = days[todayDate.getDay()];
+  const month = months[todayDate.getMonth()];
+  const date = day + ", " + month + " " + todayDate.getDate();
+
   return (
     <div className="flex flex-col gap-4 items-center">
-      <span className="text-slate-800 text-lg">Wednesday, May 24</span>
+      <span className="text-slate-800 text-lg">{date}</span>
       <p className="text-slate-800 text-4xl font-semibold">Hello, John</p>
 
       <div className="flex px-5 py-4 shadow-xl rounded-full bg-gray-200">
