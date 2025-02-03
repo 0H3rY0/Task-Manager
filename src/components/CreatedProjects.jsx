d5150ddacaef50dcc7814eefcf3b1640128a90c8
Szymon <szymers12345@gmail.com>
2025-02-03 17:57:31 +0100

    createdProject support

---
+++ src/components/CreatedProjects.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { MdOutlinePending } from "react-icons/md";
+import ProjectService from "../service/api/projects";
+
+const CreatedProjects = () => {
+  const [allProjects, setAllProjects] = useState([]);
+  const [dependProjects, setDependProjects] = useState([]);
+
+  useEffect(() => {
+    const getAllProjects = async () => {
+      const response = await ProjectService.getAll();
+      setAllProjects(response);
+    };
+
+    getAllProjects();
+  }, []);
+
+  useEffect(() => {
+    createdProjectsStats(30);
+  }, [allProjects]);
+
+  const createdProjectsStats = (lastDayCreatedAt = 7) => {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    const lastUpdatedAt = new Date(today);
+    lastUpdatedAt.setDate(today.getDate() - lastDayCreatedAt);
+
+    setDependProjects(
+      allProjects.filter((project) => {
+        const projectCreateDate = new Date(project.CreatedAt);
+        projectCreateDate.setHours(0, 0, 0, 0);
+
+        return projectCreateDate > lastUpdatedAt;
+      })
+    );
+  };
+
+  return (
+    <p className="font-semibold text-slate-800 px-6 py-1 flex items-center justify-center gap-2 ">
+      <span className="font-bold text-2xl flex justify-center items-center gap-1">
+        <MdOutlinePending size={30} /> {dependProjects.length}
+      </span>
+      Created projects
+    </p>
+  );
+};
+
+export default CreatedProjects;
--- src/components/HomeStatisticsBar.jsx
+++ src/components/HomeStatisticsBar.jsx
@@ -1,7 +1,7 @@
-import { MdOutlinePending } from "react-icons/md";
 import OwnSelectDayList from "./ui/OwnSelectDayList";
 import WelcomeUsername from "./ui/WelcomeUsername";
 import CompletedTasks from "./CompletedTasks";
+import CreatedProjects from "./CreatedProjects";
 
 const HomeStatisticsBar = () => {
   return (
@@ -10,12 +10,7 @@ const HomeStatisticsBar = () => {
       <div className="flex px-5 py-4 shadow-xl rounded-full bg-gray-200">
         <OwnSelectDayList />
         <CompletedTasks />
-        <p className="font-semibold text-slate-800 px-6 py-1 flex items-center justify-center gap-2 ">
-          <span className="font-bold text-2xl flex justify-center items-center gap-1">
-            <MdOutlinePending size={30} /> 1
-          </span>{" "}
-          Created projects
-        </p>
+        <CreatedProjects />
       </div>
     </div>
   );
